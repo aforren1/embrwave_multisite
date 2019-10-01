1cb87b84c0aa6181af3522155dcde347a5566a4b
Alex Forrence <aforren1@jhu.edu>
2019-10-01 10:30:31 -0400

    dv03

--- embr_survey/dvs/__init__.py
+++ embr_survey/dvs/__init__.py
@@ -1,4 +1,5 @@
 from .dv01 import DV01SimilarityObjects
 from .dv02 import DV02Belonging
+from .dv03 import DV03Utilitarian
 from .dv04 import DV04Nostalgia
 from .dv09 import DV09Loneliness
--- embr_survey/dvs/dv03.py
+++ embr_survey/dvs/dv03.py
@@ -33,3 +33,42 @@ class DV03Utilitarian(BaseDv):
         random.shuffle(self.questions)
 
         # one question block per page
+        self.qblocks = []
+        for (count, subprompt, question) in self.questions:
+            self.qblocks.append(QuestionBlock(win, subprompt, header=header,
+                                              questions=[question]))
+
+    def run(self, temperature):
+        # fade in
+        now = datetime.now().strftime('%y%m%d-%H%M%S')
+        answers = []
+        for qblock in self.qblocks:
+            done = False
+            while not done:
+                current_answers = qblock.update()  # in this case, should just be one
+                no_nones = not any(ca is None for ca in current_answers)
+                done = ok_button(self.win.impl.reg_font, no_nones)
+                self.win.flip()
+            # TODO: fade between questions
+            answers.extend(current_answers)
+        # save data, fade out
+        settings = self.settings
+        csv_name = os.path.join(settings['data_dir'], '%s_%s.csv' % (self.short_name, now))
+        num_q = len(self.questions)
+        data = {'participant_id': num_q * [settings['id']],
+                'datetime_start_exp': num_q * [settings['datetime_start']],
+                'datetime_start_block': num_q * [now],
+                'language': num_q * [settings['language']],
+                'locale': num_q * [settings['locale']],
+                'seed': num_q * [settings['seed']],
+                'questions': [q[1] for q in self.questions],
+                'question_original_order': [q[0] for q in self.questions],
+                'responses': answers,
+                'dv': num_q * [self.name],
+                'block_number': num_q * [self.block_num],
+                'embr_temperature': num_q * [temperature]}
+        keys = sorted(data.keys())
+        with open(csv_name, "w") as f:
+            writer = csv.writer(f, delimiter=",")
+            writer.writerow(keys)
+            writer.writerows(zip(*[data[key] for key in keys]))
