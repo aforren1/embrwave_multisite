b82f99cca370a381903bc76f3853c1ec20ef6508
Alex Forrence <aforren1@jhu.edu>
2019-09-30 20:24:10 -0400

    add 09

--- embr_survey/dvs/__init__.py
+++ embr_survey/dvs/__init__.py
@@ -1,3 +1,4 @@
 from .dv01 import DV01SimilarityObjects
 from .dv02 import DV02Belonging
 from .dv04 import DV04Nostalgia
+from .dv09 import DV09Loneliness
---
+++ embr_survey/dvs/dv09.py
@@ -0,0 +1,6 @@
+from embr_survey.dvs.base_dv import SimpleDV
+
+
+class DV09Loneliness(SimpleDV):
+    short_name = 'dv09'
+    name = 'dv09_loneliness'
