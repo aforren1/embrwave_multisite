a9d555895958aa21b4851c8dcb36e43578682251
Alex Forrence <aforren1@jhu.edu>
2019-10-23 19:59:20 -0400

    demonstrate adjustable conditional widgets

--- scratch/wid.py
+++ scratch/wid.py
@@ -37,6 +37,30 @@ class RadioGroupQ(qtw.QWidget):
 
         self.setLayout(layout)
 
+class ConditionalWidget(qtw.QWidget):
+    # pair of widgets-- one "main" question, one hidden one
+    def __init__(self, main_widget, hidden_widget, valid):
+        super().__init__()
+        self.main_widget = main_widget
+        self.valid = valid
+        main_widget.resp.buttonClicked.connect(self.check_valid)
+        self.hidden_widget = hidden_widget
+        self.hidden_widget.setHidden(True)  # hide until resp
+        layout = qtw.QVBoxLayout()
+        layout.addWidget(self.main_widget)
+        layout.addWidget(self.hidden_widget)
+        self.setLayout(layout)
+        self.setSizePolicy(qtw.QSizePolicy.Expanding, qtw.QSizePolicy.Expanding)
+    
+    def check_valid(self):
+        val = self.main_widget.resp.checkedButton().text()
+        print(val)
+        if val in self.valid:
+            self.hidden_widget.setHidden(False)
+        else:
+            self.hidden_widget.setHidden(True)
+        self.parentWidget().parentWidget().adjustSize()
+
 
 if __name__ == '__main__':
     from PySide2.QtWidgets import QApplication
@@ -44,9 +68,20 @@ if __name__ == '__main__':
 
     app = QApplication([])
     intro_dlg = RadioGroupQ('What is your name?', ['0', '1', '2', '3', '4', '5', '6', '7+'])
+    optional = RadioGroupQ('What is your quest?', ['Grail', 'Jelly Doughnut'])
     # note to self: need at least one widget to start
     # (though this is probably pretty easy to resolve)
-    window = MainWindow([intro_dlg])
+    both = ConditionalWidget(intro_dlg, optional, valid = ['1', '2', '3', '4', '5', '6', '7+'])
+
+    foob = RadioGroupQ('What is your favorite number?', ['A', 'B', 'C'])
+    opt2 = RadioGroupQ('Are you sure?', ['Yes', 'No'])
+    both2 = ConditionalWidget(foob, opt2, valid=['C'])
+    combo = qtw.QWidget()
+    layout = qtw.QVBoxLayout()
+    layout.addWidget(both)
+    layout.addWidget(both2)
+    combo.setLayout(layout)
+    window = MainWindow([combo])
     # all of the work is done by on_exit
     # of the IntroDlg
     # main loop
